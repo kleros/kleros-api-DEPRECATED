044624881991bcd9307e4e6f347057fc251e4c2c
Sam Vitello <sam.j.vitello@gmail.com>
2017-10-16 12:03:28 -0700

    getDisputeById endpoint. move mock disputes out of KlerosWrapper

--- src/kleros.test.js
+++ src/kleros.test.js
@@ -3,6 +3,7 @@ import Web3 from 'web3'
 import contract from 'truffle-contract'
 import {LOCALHOST_PROVIDER} from '../constants'
 import config from '../config'
+import mockDisputes from '../contract_wrapper/mockDisputes'
 
 
 describe('Kleros', () => {
@@ -37,67 +38,15 @@ describe('Kleros', () => {
   })
 
   test('get disputes', async () => {
-    const disputes = [
-      {
-        title: 'Unknown website owner',
-        category: 'Category',
-        deadline: '28/8/2017',
-        caseId: '#1',
-        status: 'Vote',
-        evidence: ''
-      },
-      {
-        title: 'Uncomplete software product',
-        category: 'Web, Ecommerce',
-        deadline: '28/8/2017',
-        caseId: '#2',
-        status: 'Opportunity to appeal',
-        evidence: ''
-      },
-      {
-        title: 'Unknown website owner',
-        category: 'Web, Ecommerce',
-        deadline: '10/9/2017',
-        caseId: '#3',
-        status: 'Execution',
-        evidence: ''
-      },
-      {
-        title: 'Stolen logo',
-        category: 'Category',
-        deadline: '28/8/2017',
-        caseId: '#4',
-        status: 'Execution',
-        evidence: ''
-      },
-      {
-        title: 'Unknown website owner',
-        category: 'Category',
-        deadline: '28/8/2017',
-        caseId: '#5',
-        status: 'Vote',
-        evidence: ''
-      },
-      {
-        title: 'Stolen logo',
-        category: 'Category',
-        deadline: '28/8/2017',
-        caseId: '#6',
-        status: 'Vote',
-        evidence: ''
-      },
-      {
-        title: 'Stolen logo',
-        category: 'Category',
-        deadline: '28/8/2017',
-        caseId: '#7',
-        status: 'Vote',
-        evidence: ''
-      }
-    ]
-
     let disputesKleros = await court.getDisputes()
 
-    expect(disputes).toEqual(disputesKleros);
+    expect(mockDisputes).toEqual(disputesKleros);
+  })
+
+  test('get dispute by id', async () => {
+    const testDispute = mockDisputes[0]
+    let disputeKleros = await court.getDisputeById(testDispute.caseId)
+
+    expect(testDispute).toEqual(disputeKleros);
   })
 })
