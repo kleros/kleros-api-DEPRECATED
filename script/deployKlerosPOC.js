3c2e14de53134345cc7b468708e61f4af9bc395e
Sam Vitello <sam.j.vitello@gmail.com>
2017-11-03 14:42:28 -0700

    take out hardcoded addr

--- script/deployKlerosPOC.js
+++ script/deployKlerosPOC.js
@@ -1,7 +1,7 @@
 import Kleros from '../src/Kleros'
 import Web3 from 'web3'
 import contract from 'truffle-contract'
-import {LOCALHOST_ETH_PROVIDER, LOCALHOST_STORE_PROVIDER} from '../constants'
+import {LOCALHOST_ETH_PROVIDER, LOCALHOST_STORE_PROVIDER, RNG_ADDRESS} from '../constants'
 import config from '../config'
 
 let court
@@ -19,7 +19,7 @@ let deployKlerosPOC = async () => {
   // console.log('rng address: ', RNG.address)
   court = await KlerosInstance.court
   const klerosCourt = await court.deploy(
-    "0x2829423d0dcF790cD692B0e6CE72e4553bA7594F",
+    RNG_ADDRESS,
     PNK.address
   )
   console.log('Kleros POC court address: ', klerosCourt.address)
