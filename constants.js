2f84955a3302b87247df78d7b186e78a347a3e97
Sam Vitello <sam.j.vitello@gmail.com>
2017-12-01 17:54:37 -0800

    split up execute and redistribute

--- constants.js
+++ constants.js
@@ -3,3 +3,4 @@ export const LOCALHOST_STORE_PROVIDER = 'https://kleros.in'
 export const RNG_ADDRESS = ''
 export const DISPUTE_STATUS = 3
 export const VOTING_PERIOD = 2
+export const DISPUTE_STATE_INDEX = 6
