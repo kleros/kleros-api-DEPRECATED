ad0a6ae418f71442f4496158fbf94cfa7f9e48c4
Sam Vitello <sam.j.vitello@gmail.com>
2018-02-26 17:08:36 -0500

    remove unused dependencies

--- constants.js
+++ constants.js
@@ -1,5 +1,4 @@
 export const LOCALHOST_ETH_PROVIDER = 'http://localhost:8545'
-// export const LOCALHOST_STORE_PROVIDER = 'http://localhost:3001'
 export const LOCALHOST_STORE_PROVIDER = 'https://kleros.in'
 export const RNG_ADDRESS = ''
 export const DISPUTE_STATUS = 3
