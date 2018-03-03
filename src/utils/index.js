dcae55569109477394c9126cebe23f19d8b6d0d2
zetsingithub <zetsin@gmail.com>
2018-03-03 23:49:08 +0800

    handle case of multiple outputs

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,16 +1,23 @@
 import Web3 from 'web3'
 
-const Parsers = {
-  // Types
-  types: {
-    uint8: v => v.toNumber(),
-    uint256: v => v.toNumber(),
-  },
-  // Names
-  names: {
-    partyAFee: v => Web3.fromWei(v, 'ether'),
-    partyBFee: v => Web3.fromWei(v, 'ether'),
+const parse = (type, name, value) => {
+  const parsers = {
+    // Types
+    types: {
+      uint8: v => v.toNumber(),
+      uint256: v => v.toNumber(),
+    },
+    // Names
+    names: {
+      partyAFee: v => Web3.fromWei(v, 'ether'),
+      partyBFee: v => Web3.fromWei(v, 'ether'),
+    }
   }
+
+  let result = value
+  if (parsers.types[type]) result = parsers.types[type](result)
+  if (parsers.names[name]) result = parsers.types[name](result)
+  return result
 }
 
 export function setABIGetters(context, abiArray) {
@@ -21,18 +28,7 @@ export function setABIGetters(context, abiArray) {
 
       let result = await instance[abi.name].call()
 
-      if(abi.outputs.length === 1) {
-        const output = abi.outputs[0]
-
-        result = Parsers.types[output.type] && Parsers.types[output.type](result)
-
-        result = Parsers.names[abi.name] && Parsers.names[abi.name](result)
-      }
-      else {
-        // FIXME: do we need to handle multiple outputs?
-      }
-
-      return result
+      return abi.outputs.length === 1 ? parse(abi.outputs[0].type, abi.name, result) : abi.outputs.map(({ type }, i) => parse(type, abi.name, result[i]))
     }
   })
 }
