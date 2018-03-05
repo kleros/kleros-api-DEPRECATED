2437737795ffc79eda3073952ec36a60ad2da6b5
epiqueras <epiquerass@gmail.com>
2018-03-05 13:30:16 -0800

    chore(webpack): fix filename in dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 const { resolve } = require('path')
 
-const libraryName = 'kleros-api'
 module.exports = env => ({
   entry: './src/index.js',
   output: {
     path: resolve(__dirname, 'lib/'),
-    filename: env.NODE_ENV === 'production' ? libraryName + '.js' : undefined,
+    filename: 'kleros-api.js',
     libraryTarget: 'umd',
     umdNamedDefine: true,
-    library: libraryName
+    library: 'kleros-api'
   },
 
   devtool: 'source-map',
